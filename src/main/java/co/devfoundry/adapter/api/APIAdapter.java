ec6741e4259d344ad7e7667e4a12fc359572124e
dijx <dijxie@gmail.com>
2019-06-10 16:50:01 +0200

    commit

--- src/main/java/co/devfoundry/adapter/api/APIAdapter.java
+++ src/main/java/co/devfoundry/adapter/api/APIAdapter.java
@@ -6,24 +6,30 @@ import java.time.LocalDate;
 
 public class APIAdapter implements LibraryAPI {
 
-    LibraryAPIv2 libraryAPIv2;
-    User user;
+    private LibraryAPIv2 libraryAPIv2;
+    private User user;
 
     @Override
     public boolean isAviable(String bookTitle) {
-        System.out.println("nie ma książki");
+        System.out.println(libraryAPIv2.numberOfAviableCopies(bookTitle));
+        if (libraryAPIv2.numberOfAviableCopies(bookTitle) > 0) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public LocalDate dueDate(String bookTitle, String pesel) {
-        return null;
+
+        return libraryAPIv2.dueDate(bookTitle, user.getFirstName(), user.getLastName(), user.getDateOfBirth());
     }
 
     @Override
     public boolean reserve(String bookTitle, String pesel) {
-        return false;
+
+        return libraryAPIv2.reserve(bookTitle, user.getFirstName(), user.getLastName(), user.getDateOfBirth());
     }
+
 
     public APIAdapter(LibraryAPIv2 libraryAPIv2, User user) {
         this.libraryAPIv2 = libraryAPIv2;
